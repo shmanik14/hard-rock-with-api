186e97bc03da25249f58fe5d187d1a51343cf1f7
sahadat <shmanik14@yahoo.com>
2021-02-09 18:57:15 +0600

    Dynamic with API

---
+++ app.js
@@ -0,0 +1,44 @@
+document.getElementById('submit').addEventListener('click', function(){
+    const searchValue = document.getElementById('search-value').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchValue}`;
+    //console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySong(data.data))
+})
+
+const displaySong = songs => {
+    const songContainer = document.getElementById('song-container');  
+    songContainer.innerHTML = '';
+    songs.forEach(song => {
+        const mainDiv = document.createElement('div');
+        mainDiv.className = 'single-result row align-items-center my-3 p-3';
+        html = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+            <source src="${song.preview}" type="audio/mpeg">
+            </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>
+        `;
+        mainDiv.innerHTML = html;
+        songContainer.appendChild(mainDiv);
+    })
+    
+}
+
+const getLyrics = (name, title) =>{
+    const url2 = `https://api.lyrics.ovh/v1/${name}/${title}`;
+    fetch(url2)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+}
+
+const displayLyrics = (lyrics) =>{
+    const getLyrics = document.getElementById('lyrics');
+    getLyrics.innerText = lyrics;
+}
